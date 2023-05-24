c979895c861826da1e9363823972098ad59a599c
Chenschenschen <134524990+Chenschenschen@users.noreply.github.com>
2023-05-24 22:56:30 +0800

    Add files via upload
    Chens TA test

---
+++ TA10_p1.py
@@ -0,0 +1,11 @@
+class Card:
+	def __init__(self, suit, rank):
+		self.suit = suit
+		self.rank = rank
+
+	def __str__(self):
+		# print(self.suit, self.rank) 錯了
+		return f'{self.suit} {self.rank}'
+
+card_1 = Card('Diamond', 12)
+print(card_1)
