540dfbcc68dd4fd026b49136c7f06cf59a498565
Jay <41041228s@gapps.ntnu.edu.tw>
2023-05-27 22:01:18 +0800

    Update HomePage

--- HomePage.py
+++ HomePage.py
@@ -5,35 +5,26 @@
 class HomePage():
     def __init__(self, win):
         self.win = win
-
-    def create_lb1 (self):
-        self.lb1 = tk.Label(self.win, text='一些功能介紹', fg='black', bg='#f2d5a3', font=('Arial', 12), width=23, height=2)
-        self.lb1.grid(column=1, row=10)  
-    
-    def create_start(self):
-        self.start = tk.Button(self.win, text='START', bg='#e3d5ca', fg='black', font=('Arial', 12), width=20, height=2,
-                activebackground='black', activeforeground='yellow', command=self.conceal1)
-        self.start.grid(column=1, row=50)
-
-    def create_lbtitle(self):
         self.lbtitle = tk.Label(self.win, text='\n一起聚餐吧\n', fg='black', bg='#f2d5a3', font=('Arial', 22), width=30, height=3,
                    justify='center')
-        self.lbtitle.grid(column=1, row=0)
-
+        self.start = tk.Button(win, text='START', bg='#e3d5ca', fg='black', font=('Arial', 12), width=20, height=2,
+                activebackground='black', activeforeground='yellow', command= self.conceal1)
+        self.lb1 = tk.Label(win, text='一些功能介紹', fg='black', bg='#f2d5a3', font=('Arial', 12), width=23, height=2)
 
     def show1(self):
-        self.create_lb1()
-        self.create_lbtitle()
-        self.create_start()
-
+        self.lbtitle.grid(column=1, row=0)
+        self.start.grid(column=1, row=50)
+        self.lb1.grid(column=1, row=10)  
+    
     def conceal1(self):
-        self.lbtitle.grid_remove()
-        self.start.grid_remove()
-        self.lb1.grid_remove()
+        self.lbtitle.grid_forget()
+        self.start.grid_forget()
+        self.lb1.grid_forget()
     
 
-if __name__ == '__main__':
-    import tkinter as tk
+import tkinter as tk
+
+if __name__ = '__main__':
 
     win1 = tk.Tk()
     win1.geometry('1280x720')
