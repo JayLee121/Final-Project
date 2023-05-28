0a428de59dc78ce7b3f3cc272b69f16ea252bf4f
rowanlo <134523587+rowanlo@users.noreply.github.com>
2023-05-28 22:13:35 +0800

    調整版面配置

--- user_name.py
+++ user_name.py
@@ -7,33 +7,36 @@ class UserName():
         self.win = win
         self.cb = cb
         
-        self.components = {
-            'lb3_1' : tk.Label(win, text='你的名字：', fg='black',bg ='#f5ebe0', font=('Arial', 12), width=20, height=2),
-            'et3' : tk.Entry(win, show = None),
-            'lbtitle' : tk.Label(win, text='\n一起聚餐吧\n', fg='black', bg='#f2d5a3', font=('Arial', 22), width=30, height=3,
-                                justify='center'),
-            'lb3_4' : tk.Label(win, text='已填好的人', fg='black',bg ='#f5ebe0', font=('Arial', 12), width=20, height=2),
-            'lb3_5' : tk.Label(win, text='some user name', fg='black',bg ='#f5ebe0', font=('Arial', 12), width=30, height=5),
-            'bt3': tk.Button(win, text='我填好了，換下一位！', bg='#e3d5ca', fg='black', font=('Arial', 12), width=20, height=2, activebackground='black', activeforeground='yellow', command = self.cb),
+            self.components = {
+            'lb3_1': tk.Label(win, text='你的名字：', fg='black', bg='#f5ebe0', font=('Arial', 12), width=20, height=2),
+            'et3': tk.Entry(win, show=None),
+            'lbtitle': tk.Label(win, text='\n一起聚餐吧\n', fg='black', bg='#f2d5a3', font=('Arial', 22, "bold"), width=30,
+                                height=3, justify='center'),
+            'lb3_4': tk.Label(win, text='已填好的人', fg='black', bg='#f2d5a3', font=('Arial',12, "bold"), width=30, height=2),
+            'lb3_5': tk.Label(win, text='other username', fg='grey', bg='#f5ebe0', font=('Arial', 10), width=30,
+                              height=5),
+            'bt3': tk.Button(win, text='我填好了，換下一位！', bg='#e3d5ca', fg='black', font=('Arial', 12), width=20,
+                             height=2, activebackground='black', activeforeground='yellow', command=self.cb),
         }
-    
-    def latout(self):
-        self.components['lb3_1'].grid(column=0, row=3)
-        self.components['et3'].grid(column=1, row=3)
-        self.components['lbtitle'].grid(column=1, row=0)
-        self.components['lb3_4'].grid(column=0, row=2)
-        self.components['lb3_5'].grid(column=1, row=2)
-        self.components['bt3'].grid(column=3, row=5)
 
-    
+    def layout(self):
+        self.components['lb3_1'].place(anchor="center", relx=0.42, rely=0.380)
+        self.components['et3'].place(anchor="center", relx=0.56, rely=0.380)
+        self.components['lbtitle'].place(anchor="center", relx=0.5, rely=0.278)
+        self.components['lb3_4'].place(anchor="center", relx=0.5, rely=0.44)
+        self.components['lb3_5'].place(anchor="center", relx=0.5, rely=0.5)
+        self.components['bt3'].place(anchor = "center", relx= 0.5, rely = 0.65)
+
+
     def show(self):
+        self.layout()
         for _, item in self.components.items():
-            item.grid()
-    
+            item.place()
+
     def hide(self):
         for _, item in self.components.items():
-            item.grid_remove()
-    
+            item.place_forget()
+            
     def get_result(self):
         name = self.et3.get()
         return name
