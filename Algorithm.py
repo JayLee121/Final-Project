391f71f5d7d1e7932ebc9dbdea7514fc5243fbe3
boyulin35 <boyulin35@gmail.com>
2023-05-28 16:08:00 +0800

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -1,5 +1,4 @@
 # Algorithm
-
 # 幫我整理好 time_dict 的 key = time  value = list.append(user's name)
 # 在傳入 time_dict 的時候要先切成每半小時為單位，key存成約的時間“點”（例如18:00)
 # site_dict key = use's name / value = list.append(site_name)
@@ -7,13 +6,9 @@
 debug = True
 time_proportion = 1/2
 site_proportion = 1/3
-total_people = 3
-
-'''time_dic = {'6:00':['justin', '1243'], '6:10':['12345']}
-site_dic = {'justin': ['abc','cde'], '1243':['abc','okl'],'12345':['abc','opl'] }'''
 
 # 找出票數大於比例的 x_dict key:time or restaurant name / value :user's name 
-def filter_proportion(x_dict, proportion):
+def filter_proportion(x_dict, proportion, total_people):
     temp_x = []
     for key in x_dict.keys():
         num = len(x_dict[key])
@@ -29,10 +24,10 @@ def find_optimal(temp_x):
         if accept_x[i][0] == optimal_x[0]: optimal_x.append(accept_x[i][0])
     return optimal_x
 
-def match(time_dict, site_dict):
+def match(time_dict, site_dict, total_people):
     find_time, find_site = True, True
     # 可接受的時間
-    accept_time = filter_proportion(time_dict, time_proportion)
+    accept_time = filter_proportion(time_dict, time_proportion, total_people)
     if len(accept_time) == 0: find_time = False
     if debug: print('accept_time:', accept_time)
     
@@ -51,10 +46,7 @@ def match(time_dict, site_dict):
                     site_vote[site] = [name]
                 else:
                     site_vote[site].append(name)
-    
     # 可接受的餐廳
-    accept_site = filter_proportion(site_vote, site_proportion)
+    accept_site = filter_proportion(site_vote, site_proportion, total_people)
     if len(accept_site) == 0: find_site = False
     if debug: print('accept_site:', accept_site)
-
-match (time_dic, site_dic)
