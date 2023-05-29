0194151a8668353cb1d65298b1515b53e4735dd9
boyulin35 <boyulin35@gmail.com>
2023-05-29 18:05:37 +0800

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -1,8 +1,3 @@
-# Algorithm
-# 幫我整理好 time_dict 的 key = time  value = list.append(user's name)
-# 在傳入 time_dict 的時候要先切成每半小時為單位，key存成約的時間“點”（例如18:00)
-# site_dict key = use's name / value = list.append(site_name)
-
 debug = True
 time_proportion = 1/2
 site_proportion = 1/3
@@ -24,15 +19,15 @@ def find_optimal(temp_x):
         if accept_x[i][1] == accept_x[0][1]: optimal_x.append(accept_x[i][0])
     return optimal_x
 
-def match(time_dict, site_dict, total_people):
+def get_result(time_dict, site_dict, total_people):
     accept_time = filter_proportion(time_dict, time_proportion, total_people)  # 可接受的時間
-    if len(accept_time) == 0: return None
+    if len(accept_time) == 0: return []
     if debug: print('accept_time:', accept_time)
-    optimal_time = find_optimal(accept_time)  # 找出最佳的時間，並且判斷是否有複數個
+    optimal_time = find_optimal(accept_time)  # 找出最佳的時間
     if debug: print('optimal_time:', optimal_time)
-
+    
     # 計算這些參加者投了哪些餐廳
-    site_vote = dict()
+    site_vote = {}
     for time in optimal_time:
         name_list = time_dict[time]
         for name in name_list:
@@ -42,8 +37,30 @@ def match(time_dict, site_dict, total_people):
                     site_vote[site] = [name]
                 else:
                     site_vote[site].append(name)
-    if debug: print(site_vote)
+    
+    if debug: print('site_vote:', site_vote)
     accept_site = filter_proportion(site_vote, site_proportion, total_people)  # 可接受的餐廳
-    if len(accept_site) == 0: find_site = False
     if debug: print('accept_site:', accept_site)
+    
+    # 只有一個最佳時間 餐廳一個或多個
+    if len(optimal_time) == 1:
+        optimal_time = optimal_time[0]
+        result_list = []
+        result_list.append([optimal_time])  # 時間
+        
+        # 餐廳
+        result_list[0].append(accept_site[0][0])
+        for i in range(1, len(accept_site)):
+            if accept_site[i][1] == accept_site[0][1]:
+                result_list[0][1] += ', '
+                result_list[0][1] += accept_site[i][0]
+        result_list[0].append(time_dict[optimal_time][0])
+        for i in range(1, len(time_dict[optimal_time])):
+            result_list[0][2] += ', '
+            result_list[0][2] += time_dict[optimal_time][1]
+    
+    # 有多個最佳時間 
+    else:
+        result_list = []
+    if debug: print('result_list:', result_list)
 
