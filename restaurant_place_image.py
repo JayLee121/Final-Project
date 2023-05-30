aeeae342b3bdf617cde3d8d07c809044aee04d19
Chenschenschen <134524990+Chenschenschen@users.noreply.github.com>
2023-05-31 02:07:14 +0800

    調整排版

--- restaurant_place_image.py
+++ restaurant_place_image.py
@@ -12,9 +12,11 @@ class RestaurantPage:
         self.images = []
         self.labels = []  # 儲存超連結菜單的列表
         self.image_labels = []  # 儲存圖片的列表
-        self.components = {'bt5':tk.Button(self.win, text='Save Selected', command=self.cb),
-                           'bt6':tk.Button(self.win, text='結束填寫', command=self.over),
-                           'menulabel':tk.Label(self.win, text="點選餐廳看菜單！", fg='black', bg='#f2d5a3', font=('Arial', 20,"bold"), width=23, height=2),
+        self.components = {'bt5':tk.Button(self.win, text='下一位', font=('Arial', 16), width=20,
+                                height=2, activebackground='black', activeforeground='yellow', command=self.cb),
+                           'bt6':tk.Button(self.win, text='產生結果', font=('Arial', 16), width=20,
+                                height=2, activebackground='black', activeforeground='yellow', command=self.over),
+                           'lbtitle':tk.Label(self.win, text='\\   點餐廳名看菜單  /', fg='black', bg='#f2d5a3', font=('Arial', 30, "bold")),
                            }
         self.links = [
             {"text": "新生南路麥當勞", "url": "https://www.mcdonalds.com/tw/zh-tw/full-menu/extra-value-meals.html"},
@@ -26,7 +28,6 @@ class RestaurantPage:
         self.load_images()
     
     def load_images(self):
-        #image_paths = ["餐廳首圖_麥當勞(780x585).jpeg", "餐廳首圖_順園(4000 x 2925).webp", "餐廳首圖_辛殿(2280 x912).webp", "餐廳首圖_鍋in(600x600).jpeg", "餐廳首圖_貳樓(1920x1280).jpeg"]
         image_paths = ["餐廳首圖_麥當勞.jpg", "餐廳首圖_順園.jpg", "餐廳首圖_辛殿.jpg", "餐廳首圖_鍋in.jpg", "餐廳首圖_貳樓.jpg"]
         for path in image_paths:
             image = Image.open(path)
@@ -41,21 +42,21 @@ class RestaurantPage:
             self.selected_boxes.append(checkbox_value)
                     
     def create_checkboxes(self):
-        # 生成時間段列表
+        # 生成餐廳列表
         restaurant_list = ['新生南路麥當勞','順園小館','辛殿公館店','鍋in','貳樓公館店']
         for i in range(5):
             row = []
             for j in range(1):
                 checkbox_value = i + 1  # 計算複選框對應的值
-                checkbox = tk.Checkbutton(self.win, text=restaurant_list[checkbox_value-1], command=lambda value=checkbox_value: self.checkbox_clicked(value))
-                checkbox.place(x=20, y=60 + i * 110)  # 將複選框放置到指定的行和列
+                checkbox = tk.Checkbutton(self.win, text=restaurant_list[checkbox_value-1], font=('Arial', 16), command=lambda value=checkbox_value: self.checkbox_clicked(value))
+                checkbox.place(relx=0.25, rely=0.15 + i * 0.14)  # 將複選框放置到指定的行和列
                 row.append(checkbox)
             self.checkboxes.append(row)
 
     def layout(self):
-        self.components['bt5'].place(x=20, y=650)  # 將按鈕放置到指定的行和列
-        self.components['bt6'].place(x=120, y=650)
-        self.components['menulabel'].place(x=150 , y=10)
+        self.components['lbtitle'].place(relx=0.5, rely=0.1, anchor='center')
+        self.components['bt5'].place(relx=0.4, rely=0.9, anchor='center')
+        self.components['bt6'].place(relx=0.6, rely=0.9, anchor='center')
         self.create_checkboxes()
     
 
@@ -77,14 +78,14 @@ class RestaurantPage:
     def display_images(self):
         for i, image in enumerate(self.images):
             label = tk.Label(self.win, image=image)
-            label.place(x=400, y=60 + i * 110)
+            label.place(relx=0.63, rely=0.15 + i * 0.14)
             self.image_labels.append(label)
 
     def create_menu(self):
         # 創建菜單超連結
         for i, link in enumerate(self.links):
-            label = tk.Label(self.win, text=link["text"], fg="black", cursor="hand2", bg='#f2d5a3', font=('Arial', 12,"bold"), width=23, height=2)
-            label.place(x=200, y=60 + i * 110)
+            label = tk.Label(self.win, text=link["text"], fg="black", cursor="hand2", bg='#f2d5a3', font=('Arial', 16, "bold"))
+            label.place(relx=0.45, rely=0.15 + i * 0.14)
             label.bind("<Button-1>", lambda e, url=link["url"]: webbrowser.open_new(url))
             self.labels.append(label)
     
