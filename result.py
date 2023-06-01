608a2a47f160931901efeb7391003111dbba70af
Chenschenschen <134524990+Chenschenschen@users.noreply.github.com>
2023-06-01 20:03:46 +0800

    排版：兩結果一頁、label變寬

--- result.py
+++ result.py
@@ -22,12 +22,22 @@ class ResultPage():
         self.components['title'].place(anchor="center", relx=0.5, rely=0.2)
         self.components['result_num'].place(anchor="center", relx=0.5, rely=0.3)
         
-        if len(self.rs_list) <= 4 or (len(self.rs_list) > 5 and self.rs_list[-1]== []) :
+        #if len(self.rs_list) <= 4 or (len(self.rs_list) > 5 and self.rs_list[-1]== []) :
+        if len(self.rs_list) <= 2 or (len(self.rs_list) > 3 and self.rs_list[-1]== []) :
             self.components['back_to_homepage'].place(anchor="center", relx=0.5, rely=0.82)
         else:
             self.components['back_to_homepage'].place(anchor="center", relx=0.4, rely=0.82)
             self.components['more_result'].place(anchor="center", relx=0.6, rely=0.82)
 
+    '''def layout(self):
+        self.components['title'].place(anchor="center", relx=0.5, rely=0.2)
+        self.components['result_num'].place(anchor="center", relx=0.5, rely=0.3)
+        
+        if len(self.rs_list) <= 4 or (len(self.rs_list) > 5 and self.rs_list[-1]== []) :
+            self.components['back_to_homepage'].place(anchor="center", relx=0.5, rely=0.82)
+        else:
+            self.components['back_to_homepage'].place(anchor="center", relx=0.4, rely=0.82)
+            self.components['more_result'].place(anchor="center", relx=0.6, rely=0.82)'''
 
     def create_result(self):
         # 最終結果的label
@@ -36,8 +46,47 @@ class ResultPage():
         if self.rs_list  == []:
             result_lb = tk.Label(self.win, text='大家時間對不上，約不成了嗚嗚', fg='black', bg='#f8ecc9', font=('Arial', 16), width=30, height=6)
             result_lb.place(anchor="center", relx=0.5, rely=0.5)
-            self.result_lbs.append(result_lb)  # 好像是隱藏時要用的 參考time_table.py!
+            self.result_lbs.append(result_lb)
+ 
+        # 約成
+        else:
+            for i in range(len(self.rs_list )):
+                if self.rs_list [i] == []:
+                    continue
 
+                else:
+                    result_lb = tk.Label(self.win, 
+                                            text=f'''
+第 {i+1} 組最佳結果
+最佳時段：{self.rs_list [i][0]}
+最佳餐廳：{self.rs_list [i][1]}
+可參加者：{self.rs_list [i][2]}
+''',
+                                            justify='left', bg='#f8ecc9', font=('Arial',16), width=60, height=6)
+                    
+                    if (i+1) % 2 == 1:
+                        result_lb.place(relx=0.5, rely=0.455, anchor='center')
+                    elif (i+1) % 2 == 0:
+                        result_lb.place(relx=0.5, rely=0.655, anchor='center')
+                    
+                    self.result_lbs.append(result_lb)
+                    self.rs_list [i] = []  # 把已顯示過的結果轉為空的list
+
+                if (i + 1) % 2 == 0:  # 已顯示四個結果
+                    break
+        
+        return self.rs_list 
+
+
+    """def create_result(self):
+        # 最終結果的label
+
+        # 約不成
+        if self.rs_list  == []:
+            result_lb = tk.Label(self.win, text='大家時間對不上，約不成了嗚嗚', fg='black', bg='#f8ecc9', font=('Arial', 16), width=30, height=6)
+            result_lb.place(anchor="center", relx=0.5, rely=0.5)
+            self.result_lbs.append(result_lb)
+ 
         # 約成
         else:
             for i in range(len(self.rs_list )):
@@ -69,7 +118,7 @@ class ResultPage():
                 if (i + 1) % 4 == 0:  # 已顯示四個結果
                     break
         
-        return self.rs_list 
+        return self.rs_list""" 
 
     def show(self):
         self.create_result()
@@ -99,24 +148,33 @@ if __name__ == '__main__':
 
     # 測資
     
-    # rs_list  = [['time1', 'rest1', 'name1'], 
-    #                 ['time2', 'rest2', 'name2'], 
-    #                 ['time3', 'rest3', 'name3']]
+    '''rs_list  = [['time1', 'rest1', 'name1'], 
+                     ['time2', 'rest2', 'name2'], 
+                     ['time3', 'rest3', 'name3']]'''
     
     rs_list  = [['time1', 'rest1', 'name1'], 
                     ['time2', 'rest2', 'name2'], 
                     ['time3', 'rest3', 'name3'], 
                     ['time4', 'rest4', 'name4'], 
                     ['time5', 'rest5', 'name5'], 
-                    ['time6', 'rest6', 'name6'], 
-                    ['time7', 'rest7', 'name7'], 
-                    ['time8', 'rest8', 'name8'], 
-                    ['time9', 'rest9', 'name9']]
+                    #['time6', 'rest6', 'name6'], 
+                    #['time7', 'rest7', 'name7'], 
+                    #['time8', 'rest8', 'name8'], 
+                    #['time9', 'rest9', 'name9']
+                    ]
 
-    #self.rs_list  = [['18:00-20:00', '麥當勞公館店', 'Amber, Rowan, Jay'], ['11:00-15:00', '辛殿', 'Rowan, Jay, Celest'], ['17:00-21:00', '順園小館', 'Celest, Rowan'],['15:00-18:00', '貳樓', 'Amber, Celest']]
-    # print(rs_list)
+    rs_list  = [['time1', '新生南路麥當勞,順園小館,辛殿公館店,鍋in,貳樓公館店', 'name1name1name1name1name1'], 
+                    ['time2', '新生南路麥當勞,順園小館,辛殿公館店', 'name2name2name2name2'], 
+                    ['time3', '順園小館,辛殿公館店', 'name3name3name3name3name3name3'], 
+                    ['time4', 'rest4', 'name4'], 
+                    ['time5', 'rest5', 'name5'], 
+                    #['time6', 'rest6', 'name6'], 
+                    #['time7', 'rest7', 'name7'], 
+                    #['time8', 'rest8', 'name8'], 
+                    #['time9', 'rest9', 'name9']
+                    ]
 
-    # rs_list = []
+    #rs_list = []
 
     def cb():
         page.hide()
